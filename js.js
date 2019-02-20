de6c937dd4e368027e6a48658b7105238f859b49
Luis <luismurcia97@gmail.com>
2019-02-20 12:33:18 +0100

    LuisMurcia

---
+++ js.js
@@ -0,0 +1,12 @@
+
+$('#btnPrev').click(function () {
+  $('#myCarousel').carousel(1);
+});
+ 
+$('#btnPause').click(function () {
+  $('#myCarousel').carousel(2);
+});
+ 
+$('#btnNext').click(function () {
+  $('#myCarousel').carousel(3);
+})
